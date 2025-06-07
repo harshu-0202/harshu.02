2ebe4661ed3c0a240606939ef4f34340fd910065
harshu-0202 <harshuprajapati352@gmail.com>
2025-06-07 15:41:04 +0530

    07-06-2025

---
+++ L-4.py
@@ -0,0 +1,108 @@
+# python datatyps , variables and operators
+
+#vaeiables 
+
+# x = 5
+# y = "Hello"
+# z = 3.14
+
+# print(x + y + z)
+
+#a = 10
+#b = int("20")
+#print(a + b)
+
+#a = str(10)
+#b = "20"
+
+#print(a + b)
+
+#x = True
+#y = False
+
+#print(int(x))
+#print(int(y))
+
+#a = 20.21
+#b = 20.32
+
+#print(int(a + b))
+
+#print("Hello World!!!")
+
+# Arithmetic oprator
+
+# + , - , * , / , % , **
+
+x = 5
+y = 4
+
+#print(x + y)
+#print(x - y)
+#print(x * y)
+#print(x / y)
+#print(x % y)
+#print(x ** y)
+
+# Assignment Operator 
+
+# x = "hello"
+
+#x += 20 # x = x + 20 #30
+#print(x)
+#x -= 20 # x = x - 20 #-10
+#print(x)
+#x *= 5 # x = x * 5 # 50
+#print(x)
+#x /= 5 # x = x * 5 # 2
+#print(x)
+#x %= 5 # x = x * 5 # 0
+#print(x)
+# Expontiaions
+#x **= 5 # x = x * 5 # 10 x 10 x 10 x 10 x 10
+#print(x)
+
+# conditional operator
+
+# == , === ,< , > , <= , >=
+
+#x = "hello"
+#y = "hello"
+#print(x == y)
+
+#x = 40
+#y = 40
+#print(x == y)
+
+#x = "40"
+#y = "40"
+#print(x == y)
+
+#x = 40
+#y = -40
+#print(x == y)
+
+#x = 40
+#y = 40
+#print(x < y)
+#print(x > y)
+#print(x <= y)
+#print(x >= y)
+
+x , y , z = 10 , 20 , 30
+
+#print(x * z)
+#print(y - z)
+#print(z + z)
+
+# logical operator
+
+#x = True
+#y = False
+#z = False
+
+#print(x or y or z)
+
+#print(x and y and z)
+
+#print(not x)
---
+++ index.py
@@ -0,0 +1,28 @@
+# print Hello World Progrm
+
+# print ("Hello World")
+
+# print your name 
+
+#name = input("Enter Your Name: ")
+# print("Hello", name)
+
+# check positive , nagative or zero
+
+#num = int(input("Enter Your numer:"))
+
+#if num > 0:
+ #   print("Positive")
+#elif num < 0:
+ #   print("Nagative")
+#else:
+ #   print("Zero")
+
+ # check positive , nagative or zero
+
+ num = int(input("Enter your number:"))
+
+ if num > 0:
+    print("Positive")
+elif num < 0:
+    print("")
