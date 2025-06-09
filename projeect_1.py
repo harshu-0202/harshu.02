9ecd069130084f00081f38378073af252162384d
harshu-0202 <harshuprajapati352@gmail.com>
2025-06-09 12:24:15 +0530

    Add files via upload

---
+++ projeect_1.py
@@ -0,0 +1,35 @@
+print("Welcome to the Interactive personal data collector")
+
+print("Welcome to the Interactive personal data collector")
+
+name = input("Enter your name: ") 
+age = int(input("Enter your age: ")) 
+height = float(input("Enter your height in cm: "))
+favourite_number = int(input("Enter your favourite number: "))
+
+print(name)
+print(age)
+print(height)
+print(favourite_number)
+
+birth_year = 2025 - 19
+
+print("\n👤 Here is the summary of your information:")
+print(f"Name: {name}")
+print(f"Age: {age} years")
+print(f"Estimated Birth Year: {birth_year}")
+print(f"Height: {height} cm")
+print(f"Favourite Number: {favourite_number}")
+
+print("\n🧠 Type Conversion Notes:")
+print("-> Age was converted from string to integer using int().")
+print("-> Height was converted from string to float using float().")
+
+print("\n🔍 Variable Summary:")
+print(f"name -> Value: {name}, Type: {type(name)}, Memory ID: {id(name)}")
+print(f"age -> Value: {age}, Type: {type(age)}, Memory ID: {id(age)}")
+print(f"height -> Value: {height}, Type: {type(height)}, Memory ID: {id(height)}")
+print(f"favourite_number -> Value: {favourite_number}, Type: {type(favourite_number)}, Memory ID: {id(favourite_number)}")
+print(f"birth_year -> Value: {birth_year}, Type: {type(birth_year)}, Memory ID: {id(birth_year)}")
+
+print("🙏 Thank you for using the Personal Data Collector!")
