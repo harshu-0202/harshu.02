5b06a808028e1e258bec7715ba7585926ffad6ad
harshu-0202 <harshuprajapati352@gmail.com>
2025-06-14 15:16:42 +0530

    Add files via upload

---
+++ L-5/calculator.py
@@ -0,0 +1,19 @@
+# simple calculator using ladder statements
+
+operator = input("Enter an Operator(+ , - , * , /):")
+
+# perform for calculation
+
+if operator == '+':
+    num1 = float(input("Enter first Number : "))
+    num2 = float(input("Enter Second Number : "))
+    result = num1 + num2
+    print(f"sum of {num1} + {num2} = {result}")
+elif operator == '*':
+    num1 = float(input("Enter first Number : "))
+    num2 = float(input("Enter Second Number : "))
+    result  = num1 * num2
+    print(f"sum of {num1} * {num2} = {result}")
+else:
+    print(f"This Operator not found")
+        
