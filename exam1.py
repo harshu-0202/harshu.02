9e98b91cfc638b64198689bb3c80253ac0eaa2b9
harshu-0202 <harshuprajapati352@gmail.com>
2025-06-14 12:24:53 +0530

    Add files via upload

---
+++ exam1.py
@@ -0,0 +1,35 @@
+print("Welcome to the Bill Splitter App!")
+
+dec = True
+while dec:
+    a = float(input("Enter total bill amount: "))
+    print(" ")
+
+    if a <= 0 :
+        print("invalid amount , please enter right amount")
+        continue
+
+    b = int(input("Enter number of people: "))
+
+    if b <= 0:
+        print("invalid amount , please enter right amount")
+        continue
+
+    c = int(input("Enter Tip percentage (0/5/10/15/20): "))
+
+    if c <= 0 :
+        print("invalid amount , please enter right amount")
+
+    tip_amount = (c / 100 * a)*a
+    total_amount = a + b
+    per_person = a / b
+
+    print(f"tip amount: " , tip_amount)
+    print(f"total bill(with tip): " , total_amount)
+    print(f"Each person shoud pay: " , per_person)
+
+    dec = False
+    print(input("Would you like to calculate another bill? (yes/ne): "))
+    print("....")
+
+    
